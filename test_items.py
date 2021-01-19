49e85e6829539588c1a996e17bac7631afbf3b65
Evgeny.Popov <evpsky@gmail.com>
2021-01-19 18:35:42 +0200

    test update

--- test_items.py
+++ test_items.py
@@ -1,16 +1,13 @@
 import time
 
 
-
-
-class TestFindButton():
+class TestFindAddToBasketButton():
     def test_find_add_to_basket_button(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
         # Устанавливаем время ожидания
         time.sleep(30)
         # Ищем кнопку на станице
-        add_to_basket_button = browser.find_element_by_css_selector(".btn-primary")
+        add_to_basket_button = browser.find_element_by_css_selector(".btn-add-to-basket")
         # с помощью assert проверяем, что кнопка "Добавить в корзину" есть на странице
-        assert add_to_basket_button, "Add to basket button is missed"
-
+        assert add_to_basket_button, "Add to basket button is missed!"
